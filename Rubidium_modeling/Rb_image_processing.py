67ca24852159165c71f2c8bec8d760219a8aaad4
tommy.garcia@colorado.edu <tommyalexgarcia@gmail.com>
2021-05-04 12:57:40 -0600

    fourth update

--- Rubidium_modeling/Rb_image_processing.py
+++ Rubidium_modeling/Rb_image_processing.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import gavar as gv
 def imdata_flist_df():
     path_to_flist = '../data/raw_images/file_list.txt'
     return pd.read_csv(path_to_flist, delimiter='\t')
@@ -26,8 +27,43 @@ def unpack_image(flists, time=0):
                     delimiter="\t",
                     header=None
                     )
-                for snda_file in file
+                for file in category
                 ]
-            for file in flist_specific
+            for category in flist_specific
             ]
-    return shadow, null, dark, alt_processed
+    return shadows, nulls, darks, alt_processeds
+def shift_mat(mat,left = 1, direction = 0):
+    if direction == 0 and left == 1:
+        return np.append(mat[1:,:],mat[:1,:],axis=0)
+    if direction == 0 and left == 0:
+        return np.append(mat[-1:,:],mat[:-1,:],axis=0)
+    if direction == 1 and left == 1:
+        return np.append(mat[:,1:],mat[:,:1],axis=1)
+    if direction == 1 and left == 0:
+        return np.append(mat[:,-1:],mat[0,:-1],axis=1)
+
+def process_image(shadows, nulls, darks, OD_sat=2.8,f=1):
+    OD_Meas = [
+            np.log(( 1+abs( nulls[i]-darks[i] ))/( 1+abs(shadows[i]-darks[i])))
+            for i in range( len(shadows) )
+            ]
+    OD_mod = [
+            np.log( (1-np.exp(-OD_sat))/( np.exp(-OD_M)-np.exp(-OD_sat) ) )
+            for OD_M in OD_Meas
+            ]
+    OD_actual = [ OD_m + f*(1-np.exp(-OD_m)) for OD_m in OD_mod ]
+    i_max,j_max,k_max = len(OD_actual[0][0,:]), len(OD_actual[0][:,0]), len(OD_actual)
+    I,J,K = np.arange(0,i_max,2),np.arange(0,j_max,2),np.arange(0,k_max,2)
+    OD_statted = np.zeros(len(I),len(J),len(K))
+   for k in K:
+       for i in I:
+           for j in J:
+               M = np.array(OD_actual[k])
+               window = [ 
+                       M[i][j] +
+                       shift_mat(M)[i][j] +
+                       shift_mat(M,left = 0, direction = 1 )[i][j] +
+                       shift_mat(M,left = 0, direction = 0 )[i][j] +
+                       shift_mat(M,left = 1, direction = 1 )[i][j]
+                ]
+               OD_statted[i/2][j/2][k/2] = gv.gvar(np.mean(window),np.std(window))
