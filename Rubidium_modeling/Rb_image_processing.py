7ee877f2a6b297cef4636155b5773b63c07ae166
tommy.garcia@colorado.edu <tommyalexgarcia@gmail.com>
2021-05-04 10:49:42 -0600

    third update, image_processing updated

--- Rubidium_modeling/Rb_image_processing.py
+++ Rubidium_modeling/Rb_image_processing.py
@@ -4,17 +4,30 @@ def imdata_flist_df():
     path_to_flist = '../data/raw_images/file_list.txt'
     return pd.read_csv(path_to_flist, delimiter='\t')
 def unpack_image(flists, time=0):
-    leads = ['S','N','D','L']
+    path_to_files = '../data/raw_images/absorption_images/'
+    
+    leads = [" 'S' "," 'N' "," 'D' "," 'L' "]
     masks = [
-            flists['cat'] == lc for lc in leads
+            np.array(flists['cat'] == lc) &
+            np.array(flists['time(s)'] == time)
+            for lc in leads
             ]
-    flist_specific = flists['file_list'][mask]
-    col_name = np.linpace(-1,1,100)
+    flist_specific_sloppy = [
+            np.array(flists[mask]['file_list']) for mask in masks
+            ]
+    flist_specific = [
+            flist[0][5:-3].split("', '")
+            for flist in flist_specific_sloppy
+            ]
     shadow, null, dark, alt_processed = [
-            pd.read_csv(
-                    flist_specific[mask],
+                [
+                pd.read_csv(
+                    path_to_files + snda_file,
                     delimiter="\t",
-                    columns=col_name
-                    ) for mask in masks
+                    header=None
+                    )
+                for snda_file in file
+                ]
+            for file in flist_specific
             ]
     return shadow, null, dark, alt_processed
